442615c6938b0426c9eddef2c176408f5626ed07
Jayvin950 <jayvinjb2008@gmail.com>
2024-08-14 04:58:50 +0000

    description of changes

---
+++ madlibs.js
@@ -0,0 +1,21 @@
+//variables
+
+
+
+
+
+let noun1 = "Dog"
+let adjective1 - "quick"
+//add a variable that holds a number
+
+
+
+
+prompt("enter a verb")
+
+
+let story = "The" + noun1 + "jumped over" + adjective1
+
+
+console.log(output)
+console.log (11+2)
